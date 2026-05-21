7a6127d02eeb85f9fff191856119c0a10674f172
NikolajHPedersen <npede23@student.aau.dk>
2026-05-21 12:46:44 +0200

    Processing added to main

---
+++ pytorchexample/processing.py
@@ -0,0 +1,49 @@
+import torch
+import os
+import sklearn
+import json
+
+from models.xception import xception
+from dataset.dataset import no_augment_init_dataset,load_centralized_dataset
+
+
+def evaluate(batch,model, device):
+    images = batch["image"].to(device)
+    act = batch["label"]
+    with torch.no_grad():
+        pred = model(images.float())
+    pred_labels = torch.argmax(pred, dim=1)
+    
+    return act.cpu().tolist(), pred_labels.cpu().tolist()
+
+model_name = "fedavg_seed36836"
+
+model = xception()
+model.load_state_dict(torch.load(os.getcwd()+f"/finished_models/{model_name}.pt",weights_only=True))
+model.float()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+model.eval()
+
+no_augment_init_dataset()
+dataloader = load_centralized_dataset()
+
+act_y = []
+pred_y = []
+count = 1
+for batch in dataloader:
+    print(f"batch {count}")
+    act_list, pred_list = evaluate(batch, model=model, device=device)
+    act_y += act_list
+    pred_y += pred_list
+    count += 1
+
+report = sklearn.metrics.classification_report(act_y,pred_y,labels=[i for i in range(0,8)], output_dict=True)
+
+print(report)
+
+write_string = json.dumps(report)
+
+
+with open(f"{model_name}_report.json","w") as f:
+    f.write(write_string)
